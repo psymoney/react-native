d9cdce608dc9c066933fa9d6d68ef9979c6a94f0
Sangyeop Kim <sangykimiv@gmail.com>
2025-12-11 17:33:59 +0900

    chore: allow geolocation service on ios

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, StrictMode } from 'react';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <WebView
-//       source={{ uri: 'https://psymoney.online/' }}
-      source={{ uri: 'https://app.parkingwith.com/' }}
-      style={{ flex: 1 }}
-    />
+    <StrictMode>
+      <WebView
+        source={{ uri: 'https://app.parkingwith.com/' }}
+        style={{ flex: 1 }}
+        webviewDebuggingEnabled={true}
+        geolocationEnabled={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        onError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn('WebView error: ', nativeEvent);
+        }}
+
+      />
+    </StrictMode>
   );
 }
